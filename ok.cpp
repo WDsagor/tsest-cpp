f05c626f71e5b3db8af40313ab34183467f84d4e
WDsagor <sagarwpress@gmail.com>
2022-11-19 02:50:45 +0600

    first commit

---
+++ ok.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+ int num;
+ cout << "enter any Number: ";
+ cin >> num;
+ cout <<"Your Number is :"<<num;
+ getch();
+};
